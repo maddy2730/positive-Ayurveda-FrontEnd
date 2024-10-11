b071b0cbd81629d5b8a52e0f89e4a8d738efe41f
akashsharma8917@gmail.com <akashsharma8917@gmail.com>
2024-10-11 15:37:20 +0530

    changes in cart data

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -7,17 +7,20 @@ export default function Cart({ cart, removeFromCart, addToCart, removeFromCartsu
     return cart.reduce((total, item) => total + item.price * item.quantity, 0);
   };
 
+  const shippingCharges = 10; // Updated to match screenshot
+  const gstRate = 0.15; // Updated to match screenshot GST of 15%
+  const totalWithGST = calculateTotal() + shippingCharges + (calculateTotal() * gstRate);
+
   return (
-    <div className="container-fluid" style={{ backgroundColor: '#C9F9E2', padding: '20px' }}>
-      <table className="table table-bordered" style={{ backgroundColor: '#C9F9E2' }}>
-        <thead style={{ backgroundColor: '#306D51', color: '#C9F9E2' }}>
+    <div className="container-fluid" style={{ backgroundColor: '#E0F8E2', padding: '20px' }}>
+      <table className="table table-bordered" style={{ backgroundColor: '#E0F8E2' }}>
+        <thead style={{ backgroundColor: '#306D51', color: '#fff' }}>
           <tr className="text-center">
             <th>Product Image</th>
             <th>Product Name</th>
             <th>Unit Price</th>
             <th>Quantity</th>
             <th>Total</th>
-            <th>Actions</th>
           </tr>
         </thead>
         <tbody>
@@ -31,54 +34,48 @@ export default function Cart({ cart, removeFromCart, addToCart, removeFromCartsu
               <td>
                 <div className="pa-cart-quantity">
                   <button className="pa-sub" onClick={() => removeFromCartsubtract(item.id)}>-</button>
-                  <input type="number" value={item.quantity} min="1" readOnly style={{ width: '40px', border: '1px solid #306D51', backgroundColor: '#c9f9e2', textAlign: 'center' }} />
+                  <input type="number" value={item.quantity} min="1" readOnly style={{ width: '40px', border: '1px solid #306D51', backgroundColor: '#E0F8E2', textAlign: 'center' }} />
                   <button className="pa-add" onClick={() => addToCart(item)}>+</button>
                 </div>
               </td>
               <td>${item.price * item.quantity}</td>
-              <td>
-                <button className="pa-delete" onClick={() => removeFromCart(index)}>
-                  <FaTrash color="#306D51" />
-                </button>
-              </td>
             </tr>
           ))}
         </tbody>
       </table>
 
-      <div className="row" style={{ marginTop: '20px' }}>
-        <div className="col-lg-12 d-flex justify-content-end">
-          <h5><b>Sub Total:</b></h5> <h5>${calculateTotal()}</h5>
+      <div className="row mt-4">
+        <div className="col-12 d-flex justify-content-end">
+          <table className="table table-borderless" style={{ width: '40%' }}>
+            <tbody>
+              <tr>
+                <td><b>Sub Total:</b></td>
+                <td>${calculateTotal()}</td>
+              </tr>
+              <tr>
+                <td><b>Shipping Charges:</b></td>
+                <td>${shippingCharges}</td>
+              </tr>
+              <tr>
+                <td><b>GST (15%):</b></td>
+                <td>${(calculateTotal() * gstRate).toFixed(2)}</td>
+              </tr>
+              <tr>
+                <td><b>Grand Total:</b></td>
+                <td><b>${totalWithGST.toFixed(2)}</b></td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
 
-      <div className="row" style={{ marginTop: '20px' }}>
-        <div className="col-lg-8 d-flex justify-content-start">
-          <Link to="/shop" className="pa-btn" style={{ backgroundColor: '#306D51', color: '#fff', padding: '10px 20px', textDecoration: 'none' }}>Continue Shopping</Link>
+      {/* Buttons */}
+      <div className="row mt-4">
+        <div className="col-lg-6">
+          <Link to="/shop" className="btn btn-dark" style={{ backgroundColor: '#306D51', color: '#fff', padding: '10px 20px' }}>Continue Shopping</Link>
         </div>
-      </div>
-
-      <div className="row" style={{ marginTop: '20px', borderTop: '1px solid #306D51', paddingTop: '20px' }}>
-        <div className="col-lg-12 d-flex justify-content-end">
-          <h5><b>Shipping charges:</b></h5>  <h5>$20</h5>
-        </div>
-      </div>
-
-      <div className="row" style={{ marginTop: '20px' }}>
-        <div className="col-lg-12 d-flex justify-content-end">
-          <h5><b>GST:</b></h5> <h5>18%</h5>
-        </div>
-      </div>
-
-      <div className="row" style={{ marginTop: '20px' }}>
-        <div className="col-lg-12 d-flex justify-content-end">
-          <h5><b>Grand total:</b></h5> <h5>${calculateTotal() + 20}</h5>
-        </div>
-      </div>
-
-      <div className="row" style={{ marginTop: '20px' }}>
-        <div className="col-lg-12 d-flex justify-content-center">
-          <a href="checkout.html" className="pa-btn" style={{ backgroundColor: '#306D51', color: '#fff', padding: '10px 20px', textDecoration: 'none' }}>Proceed to checkout</a>
+        <div className="col-lg-6 d-flex justify-content-end">
+          <button className="btn btn-dark" style={{ backgroundColor: '#306D51', color: '#fff', padding: '10px 20px' }}>Update Cart</button>
         </div>
       </div>
     </div>
